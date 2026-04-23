5154366aebcb8cf03fa610cd496e7a69ee4a95a7
martinsharma18 <martinsharma18@gmail.com>
2026-04-23 23:10:12 +0545

    configure androidScheme to https in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.moneytrack.app',
   appName: 'Money Track',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
